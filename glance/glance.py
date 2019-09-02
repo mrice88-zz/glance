8323e26eb5fa9c8706a3cb5e763ef815244b7d85
mrice <mark.rice.jr@gmail.com>
2019-09-01 23:10:51 -0700

    added watch wrapper, and abstracted some things into a Watch class and a Look class

--- glance/glance.py
+++ glance/glance.py
@@ -1,15 +1,55 @@
 import time
-import matplotlib
-import maya
-import itertools
 import attr
+import variants
+import functools
 
 
 @attr.s
-class Glance(object):
+class Look:
+    start_time = attr.ib(type=float)
+    end_time = attr.ib(type=float, default=None)
+
+    def __attrs_post_init(self):
+        self.start_time = time.time()
+
+    @variants.primary
+    def look_time(self):
+        if self.end_time:
+            return self.end_time - self.start_time
+
+    @look_time.variant("datetime")
+    def look_time(self):
+        pass
+
+    def stop(self):
+        self.end_time = time.time()
+
+
+@attr.s
+class Watch:
+    target = attr.ib(type=str)
     start_time = attr.ib()
     end_time = attr.ib()
-    look = attr.ib(type=list, default=[])
+    looks = attr.ib(type=[Look], default=[])
+
+    def __attrs_post_init(self):
+        self.start_time = time.time()
+
+    @property
+    def last_look(self) -> Look:
+        return self.looks[-1]
+
+    def stop(self):
+        for look in self.looks:
+            look.stop()
+        self.end_time = time.time()
+
+
+@attr.s
+class Glance:
+    start_time = attr.ib()
+    end_time = attr.ib()
+    watches = attr.ib(type={}, default={})
     current_start = attr.ib(default=None)
     current_end = attr.ib(default=None)
 
@@ -17,25 +57,25 @@ class Glance(object):
         self.start_time = time.time()
 
     def end(self):
+        for watch in self.watches.values():
+            watch.stop()
+
         self.end_time = time.time()
 
-    def start_look(self):
-        if self.current_end:
-            pass  # This needs to break.
-        else:
-            self.current_start = time.time()
+    def watch(self, func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwawrgs):
+            if func.__name__ not in self.watches.items():
+                self.watches[func.__name__] = Watch(target=func.__name__)
 
-    def end_look(self):
-        self.current_end = time.time()
+            watch = self.watches[func.__name__]
 
-        if self.current_start:
-            self.look.append(
-                {
-                    'start': self.current_start,
-                    'end': self.current_end,
-                }
-            )
+            func_output = func(*args, **kwawrgs)
 
-            self.current_start = None
-            self.current_end = None
+            watch.last_look.stop()
+
+            return func_output
+
+        return wrapper
+
 
